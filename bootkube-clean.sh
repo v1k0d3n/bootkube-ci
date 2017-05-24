02ded4625bb5f96020d0db8daa832de0c269d73d
Brandon B. Jozsa <bjozsa@jinkit.com>
2017-05-24 14:04:11 -0400

    removing unnecessary statement

--- bootkube-clean.sh
+++ bootkube-clean.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2017 The Bootkube-CI Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Cleaning up nodes is simple:
 printf "Removing bootkube environment from system..."
 { sudo systemctl stop kubelet.service
@@ -25,5 +39,3 @@ printf "Removing bootkube environment from system..."
   sudo ip link set flannel.1 down
 } &> /dev/null
 printf "\nCOMPLETE!\n"
-# Done
-Raw
