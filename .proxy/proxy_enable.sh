155a56e4849e569485b1b6a57000c1d9eacae61f
Brandon B. Jozsa <bjozsa@jinkit.com>
2017-06-06 09:35:26 -0400

    proxy: typo fix

--- .proxy/proxy_enable.sh
+++ .proxy/proxy_enable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Add proxy URL information below:
-export $PROXY_URL="<PROXY_URL>"
-export $ADD_NOPROXY_HOSTS="<ADD_NOPROXY_HOSTS>"
+export PROXY_URL="<PROXY_URL>"
+export ADD_NOPROXY_HOSTS="<ADD_NOPROXY_HOSTS>"
 # No changes required should be required below:
 export https_proxy=$PROXY_URL
 export http_proxy=$PROXY_URL
